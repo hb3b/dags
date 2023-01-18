459625311159ba9d5df53b752e10df4004560a87
Ben Hecht <benjamin.hecht@gmail.com>
2023-01-18 02:43:40 -0500

    Update tutorial.py

--- dags/tutorial.py
+++ dags/tutorial.py
@@ -5,7 +5,7 @@ import datetime as dt
 with DAG(
     dag_id="example_python_operator",
     schedule_interval="@daily",
-    start_date=dt.datetime(2022, 01, 01),
+    start_date=dt.datetime(2021, 1, 1),
 ) as dag:
         @task.virtualenv(
             task_id="virtualenv_python", requirements=["colorama==0.4.0"], system_site_packages=False
